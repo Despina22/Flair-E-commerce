30374b374f75f23140c20ad78aa3061763cbdbac
Despina22 <stankovskadespina22@gmail.com>
2023-08-21 23:48:15 +0200

    feat: Add navigation

--- Flair/src/app/core/core.module.ts
+++ Flair/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, NavigationComponent],
   imports: [CommonModule, MatIconModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, NavigationComponent],
 })
 export class CoreModule {}
