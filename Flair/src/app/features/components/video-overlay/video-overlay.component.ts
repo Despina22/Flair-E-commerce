bea89398077b1ea90484c3a59e72d5e0701b62b0
Despina22 <stankovskadespina22@gmail.com>
2023-08-22 01:14:07 +0200

    feat: Add video overlay

---
+++ Flair/src/app/features/components/video-overlay/video-overlay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-overlay',
+  templateUrl: './video-overlay.component.html',
+  styleUrls: ['./video-overlay.component.scss']
+})
+export class VideoOverlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Flair/src/app/features/features.module.ts
+++ Flair/src/app/features/features.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
-
+import { VideoOverlayComponent } from './components/video-overlay/video-overlay.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    FeaturesRoutingModule
-  ]
+  declarations: [VideoOverlayComponent],
+  imports: [CommonModule, FeaturesRoutingModule],
+  exports: [VideoOverlayComponent],
 })
-export class FeaturesModule { }
+export class FeaturesModule {}
