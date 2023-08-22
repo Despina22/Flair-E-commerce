dac45c2ac393e1d12e836302ede51620dfe510f9
Despina22 <stankovskadespina22@gmail.com>
2023-08-23 01:31:57 +0200

    feat: add login component

--- Flair/src/app/core/core.module.ts
+++ Flair/src/app/core/core.module.ts
@@ -8,11 +8,18 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomepageComponent } from './layouts/components/homepage/homepage.component';
 import { FeaturesModule } from '../features/features.module';
 import { LoginComponent } from './auth/components/login/login.component';
+import { RouterModule } from '@angular/router';
 
 const COMPONENTS = [HeaderComponent, NavigationComponent, FooterComponent];
 @NgModule({
   declarations: [...COMPONENTS, HomepageComponent, LoginComponent],
-  imports: [CommonModule, MatIconModule, MatBadgeModule, FeaturesModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatBadgeModule,
+    FeaturesModule,
+    RouterModule,
+  ],
   exports: [...COMPONENTS],
 })
 export class CoreModule {}
